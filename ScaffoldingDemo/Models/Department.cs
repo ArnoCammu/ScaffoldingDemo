154179fc160d822f7a8ebdbc77243f1f865bea0a
arno cammu <arnocammu@hotmail.com>
2020-06-29 09:21:54 +0200

    Add project files.

---
+++ ScaffoldingDemo/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScaffoldingDemo.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+    }
+}
